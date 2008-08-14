fcf0c6254296e257e5e7be3843f0e71882736632
Joseph Moore <joe.moore@gmail.com>
2008-08-13 22:36:16 -0700

    JLM: stuff

--- Rakefile
+++ Rakefile
@@ -32,3 +32,10 @@ desc "Add new files to subversion"
 task :svn_add do
    system "svn status | grep '^\?' | sed -e 's/? *//' | sed -e 's/ /\ /g' | xargs svn add"
 end
+
+begin
+  require 'vlad'
+  Vlad.load
+rescue LoadError
+  # do nothing
+end
--- app/controllers/gimme.rb
+++ app/controllers/gimme.rb
@@ -1,10 +1,10 @@
 require 'youtube_g'
 class Gimme < Application
-  cache_page :index
+  # cache_page :index
   
   def index
     client = YouTubeG::Client.new
-    @videos = client.videos_by(:max_results => 40, :query => "zz top gimme all your lov").videos
+    @videos = client.videos_by(:max_results => 6, :query => "zz top gimme all your lov").videos
     render
   end
   
